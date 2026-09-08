c829e174522fa7dc7581276a71dac8d9f5bed6b8
SabrinaJewson <sejewson@gmail.com>
2026-09-08 01:29:07 +0100

    Add translations

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginHtmlMinifierTerser } from "rsbuild-plugin-html-minifier-terser";
+import fs from "node:fs/promises";
+
+const translations: Map<string, any> = new Map();
+for (const file of await fs.readdir("translations")) {
+  if (!file.endsWith(".ts")) {
+    continue;
+  }
+  const mod = await import(`./translations/${file}`);
+  translations.set(file.slice(0, 2), mod.default);
+}
 
 export default defineConfig({
+  source: {
+    entry: {
+      ...Object.fromEntries(
+        translations.keys().map((lang) => [lang, "./src/index.ts"]),
+      ),
+    },
+  },
   html: {
     template: "src/index.ejs",
-    title: "Yuzu Dreams",
+    title: ({ entryName }) => translations.get(entryName).title,
+    templateParameters: { translations },
   },
   plugins: [pluginHtmlMinifierTerser()],
 });
---
+++ translations/en.ts
@@ -0,0 +1,3 @@
+export default {
+  title: "Yuzu Dreams",
+};
